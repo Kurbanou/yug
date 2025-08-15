802504cd7c20e06f788aadaa9bb0bbbc782f0dcc
Farid Kurbanou <kurbanou.faridun@gmail.com>
2025-08-15 09:46:45 +0300

    init commit

---
+++ front-page.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Template for custom homepage
+ */
+get_header(); ?>
+
+<main class="homepage">
+  <section class="hero">
+    <h1>Привет, новый дизайн!</h1>
+    <p>Сделано по макету Figma</p>
+  </section>
+</main>
+
+<?php get_footer(); ?>
