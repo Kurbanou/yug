6e86fdf405e806c026499d7df3e2581d65f1805b
Farid Kurbanou <kurbanou.faridun@gmail.com>
2025-08-15 14:47:36 +0300

    dsdf

--- templates/head.php
+++ templates/head.php
@@ -2,7 +2,11 @@
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link href='https://fonts.googleapis.com/css?family=Ubuntu:400,300,500,700&subset=latin,cyrillic' rel='stylesheet' type='text/css'>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css?family=Ubuntu:400,300,500,700&subset=latin,cyrillic&display=swap" rel="stylesheet">
+
+
   <?php wp_head(); ?>
-  <script src="https://maps.googleapis.com/maps/api/js?v=3.exp&key=AIzaSyAg2pFXWVXWWvodOWNTlnzXC60A6NTJYbc"></script>
+
 </head>
