387c4f49e832380767c8a542587bb7ea695e83ff
Фарид Курбанов <kf.dev@german-web.ru>
2025-08-23 09:30:52 +0300

    fix errors wc3 validator

--- base-potaga.php
+++ base-potaga.php
@@ -3,22 +3,15 @@
 use Roots\Sage\Setup;
 use Roots\Sage\Wrapper;
 
-?>
-
-<!doctype html>
+?><!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <?php get_template_part('templates/head'); ?>
 <body class="<?php
   if (is_user_logged_in()) echo 'logged-in';
-  if (get_current_blog_id() == 2) echo ' theme--green';
-  if (get_current_blog_id() == 1) echo ' theme--orange';
+  if (get_current_blog_id() == 2) echo ' theme-green';
+  if (get_current_blog_id() == 1) echo ' theme-orange';
 ?>">
 
-<!--[if IE]>
-<div class="alert alert-warning">
-  <?php _e('You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.', 'sage'); ?>
-</div>
-<![endif]-->
 
 <?php
 do_action('get_header');
